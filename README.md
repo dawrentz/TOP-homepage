25e1d7543fc573b4dea72b59fb0009ef77dc407b
dawrentz <156712569+dawrentz@users.noreply.github.com>
2025-02-12 15:04:13 -0600

    Add hover effect to cards and animation to icons.

--- README.md
+++ README.md
@@ -5,12 +5,8 @@ https://www.theodinproject.com/lessons/node-path-advanced-html-and-css-homepage
 <!--
 
 todo:
-    -set grid on smaller media
-    -kagemusha icons are small
-    -footer
-    -animate icons hover
-    -need set size restraints not on body
 
 notes:
     -mistake: need set width with children of body, not body itself
+        --use padding to control width?
  -->
--- src/index.js
+++ src/index.js
@@ -5,8 +5,27 @@ import "./style.css";
 import * as mod from "./mod.js";
 
 //declarations
+const cards = document.querySelectorAll(".card");
 
 // ====================================== Init ====================================== //
 
-//test
-console.log("from index");
+cards.forEach((card) => {
+  card.addEventListener("mouseover", () => addHoverCardStyle(card));
+  card.addEventListener("mouseout", () => removeHoverCardStyle(card));
+});
+
+function addHoverCardStyle(card) {
+  const projectPic = card.querySelector(".project-pic");
+  const projectPicTitle = card.querySelector(".project-pic-title");
+
+  projectPic.classList.add("hovered");
+  projectPicTitle.classList.add("hovered");
+}
+
+function removeHoverCardStyle(card) {
+  const projectPic = card.querySelector(".project-pic");
+  const projectPicTitle = card.querySelector(".project-pic-title");
+
+  projectPic.classList.remove("hovered");
+  projectPicTitle.classList.remove("hovered");
+}
